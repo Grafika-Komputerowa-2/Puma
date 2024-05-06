937b040aab551ee852ed2fbe1a54853e99438d59
jakstepn <jakstepniew@gmail.com>
2024-05-06 19:54:40 +0200

    Phong Lighting

---
+++ Puma/phong_frag.glsl
@@ -0,0 +1,37 @@
+#version 330 core
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform int numLights;
+uniform Light lights[100]; // Maximum number of lights
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 SurfaceColor;
+out vec4 color;
+uniform vec3 viewPos;
+void main()
+{
+    vec3 ambientColor = vec3(0.0f);
+    vec3 diffuseColor = vec3(0.0f);
+    vec3 specularColor = vec3(0.0f);
+    for (int i = 0; i < numLights; i++)
+    {
+        // Ambient
+        ambientColor += lights[i].ambient * SurfaceColor;
+        // Diffuse
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(lights[i].position - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        diffuseColor += lights[i].diffuse * diff * SurfaceColor;
+        // Specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+        specularColor += lights[i].specular * spec;
+    }
+    vec3 result = ambientColor + diffuseColor + specularColor;
+    color = vec4(result, 1.0f);
+}
